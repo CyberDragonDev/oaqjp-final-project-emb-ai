344149ee1a5df520f075b46b647992911a93ed18
CyberDragonDev <rafael.art@icloud.com>
2026-07-09 17:42:04 -0600

    Pylint corrections

--- server.py
+++ server.py
@@ -4,24 +4,25 @@ Handles routing, processing user input, and formatting API responses.
 """
 
 from flask import Flask, render_template, request
-from EmotionDetection import emotion_detector
+from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
+
 
 @app.route("/emotionDetector")
 def text_detector():
     """
-    Route that receives an input text from the frontend, analyzes its emotions 
+    Route that receives an input text from the frontend, analyzes its emotions
     using the emotion_detector package, and returns a formatted string.
     """
     text_to_analyze = request.args.get('textToAnalyze')
-    
+
     response = emotion_detector(text_to_analyze)
-    
+
     # Error handling for empty inputs
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again."
-    
+
     output_string = (
         f"For the given statement, the system response is "
         f"'anger': {response['anger']}, 'disgust': {response['disgust']}, "
@@ -29,8 +30,9 @@ def text_detector():
         f"'sadness': {response['sadness']}. "
         f"The dominant emotion is {response['dominant_emotion']}."
     )
-    
+
     return output_string
+
 
 @app.route("/")
 def render_index_page():
@@ -39,5 +41,7 @@ def render_index_page():
     """
     return render_template('index.html')
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+    
